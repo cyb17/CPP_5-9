88f0ddaa5e396db3c7ce048a5e4a5b6c545dfbfe
Yabing <yabingc3@gmail.com>
2024-05-03 13:52:19 +0200

    ...

--- CPP06_git/ex01/main.cpp
+++ CPP06_git/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yachen <yachen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 11:40:30 by yachen            #+#    #+#             */
-/*   Updated: 2024/05/03 12:06:41 by yachen           ###   ########.fr       */
+/*   Updated: 2024/05/03 13:40:39 by yachen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@
 int	main( void )
 {
 	Data	data( 42, '*' );
+	
 	Data*	ptr = &data;
+	std::cout << "int: " << ptr->getI() << "\nchar: " << ptr->getC() << '\n';
+	std::cout << std::hex << ptr << '\n';
+	
 	uintptr_t*	raw = Serializer::serialize( ptr );
-
-	std::cout << "int: " << raw->getI() << "\nchar: " << rwa;
+	std::cout << std::hex << raw << '\n';
 
 	Data*	ptr2 = Serializer::deserialize( raw );
-	
-	std::cout << ptr2->getI() << '\n';
+	std::cout << "int: " << ptr2->getI() << "\nchar: " << ptr2->getC() << '\n';
 	
 	return 0;
 }
